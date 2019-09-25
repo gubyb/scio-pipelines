5ec6c20cbf662f3d397e01ef4934b03ee8733a87
gubyb <gustavbyberg91@gmail.com>
2019-09-25 15:27:29 +0200

    Inital commit

---
+++ test-scio-pipeline/test-scio-pipeline/src/main/scala/join/JoinFiles.scala
@@ -0,0 +1,55 @@
+package join
+
+import com.spotify.scio._
+import org.apache.beam.sdk.values.KV
+
+/*
+sbt "runMain [PACKAGE].Joinfiles
+  --project=[PROJECT] --runner=DataflowRunner --zone=[ZONE]
+  --input=gs://dataflow-samples/shakespeare/kinglear.txt
+  --output=gs://[BUCKET]/[PATH]/wordcount"
+*/
+
+object JoinFiles {
+  def main(cmdlineArgs: Array[String]): Unit = {
+
+    val streamsHeader = Array("id", "user_id", "track_id")
+    val recsHeader = Array("id", "user_id", "track_id")
+
+    val (sc, args) = ContextAndArgs(cmdlineArgs)
+
+    val streams = args("streams")
+    val recs = args("recs")
+    val output = args("output")
+
+    // Read and parsecsv
+    val streamsSc = sc.textFile(streams)
+      .map(_.trim)
+      .map(_.split(",").filter(_.nonEmpty))
+      .filter(l => !streamsHeader.sameElements(l))
+      .map(l => streamsHeader.zip(l).toMap)
+      .map(row => ((row("user_id"), row("track_id")), row))
+
+    val recsSc = sc.textFile(recs)
+      .map(_.trim)
+      .map(_.split(",").filter(_.nonEmpty))
+      .filter(l => !recsHeader.sameElements(l))
+      .map(l => recsHeader.zip(l).toMap)
+      .map(row => ((row("user_id"), row("track_id")), row))
+
+    // Join and format result object
+    val resultSC = streamsSc
+      .leftOuterJoin(recsSc)
+      .map{ row =>
+        val (_, (stream, rec)) = row
+        stream + ("is_rec" -> (if (rec.isEmpty) false else true))
+      }
+
+    //Print result
+    resultSC
+      .map(_.values.mkString(","))
+      .saveAsTextFile("output/streams", numShards = 0, suffix = ".csv")
+
+    val result = sc.close().waitUntilFinish()
+  }
+}
